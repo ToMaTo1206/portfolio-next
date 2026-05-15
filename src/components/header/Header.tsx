9c83226e0c6df6a513cbe18ca1eb4acfdeff5696
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-05-15 23:35:56 +0200

    feat: add Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import Link from "next/link"
+import {faContactCard, faCubes, faDiagramProject, faHouseUser, faRoute} from "@fortawesome/free-solid-svg-icons"
+import NavLinkIcon from "@/components/header/NavLinkIcon";
+import NavLink from "@/components/header/NavLink";
+
+/**
+ * Header du portfolio
+ */
+export default function Header() {
+    return (
+        <header className="navbar bg-secondary backdrop-blur-md shadow-xl md:px-12">
+            <div className="navbar-start">
+                <Link href="/" className="hover:opacity-80 transition-opacity">
+                    <Image
+                        src="/profil/td_logo.svg"
+                        width={50}
+                        height={50}
+                        alt="Logo du site"
+                    />
+                </Link>
+            </div>
+            <div className="navbar-end">
+                <ul className="menu menu-horizontal flex md:hidden">
+                    <NavLinkIcon href={"#"} tooltip={"Page d'accueuil"} faIcon={faHouseUser} />
+                    <NavLinkIcon href={"#"} tooltip={"Parcours"} faIcon={faRoute} />
+                    <NavLinkIcon href={"#"} tooltip={"Projets"} faIcon={faDiagramProject} />
+                    <NavLinkIcon href={"#"} tooltip={"Stack technique"} faIcon={faCubes} />
+                    <NavLinkIcon href={"#"} tooltip={"Contact"} faIcon={faContactCard} />
+                </ul>
+                <ul className="menu menu-horizontal hidden md:flex">
+                    <NavLink href={"#"}>Présentation</NavLink>
+                    <NavLink href={"#"}>Parcours</NavLink>
+                    <NavLink href={"#"}>Projets</NavLink>
+                    <NavLink href={"#"}>Stack</NavLink>
+                    <NavLink href={"#"}>Contact</NavLink>
+                </ul>
+            </div>
+        </header>
+    )
+}
