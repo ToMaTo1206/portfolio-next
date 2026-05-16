38383c2ae96375533857b1943c7000500bad4ae1
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-05-16 12:52:39 +0200

    fix: z-index on the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,7 +9,7 @@ import NavLink from "@/components/header/NavLink";
  */
 export default function Header() {
     return (
-        <header className="navbar bg-secondary backdrop-blur-md shadow-xl md:px-12 sticky top-0">
+        <header className="navbar bg-secondary/85 backdrop-blur-md z-50 shadow-xl md:px-12 sticky top-0">
             <div className="navbar-start">
                 <Link href="/" className="hover:opacity-80 transition-opacity">
                     <Image
