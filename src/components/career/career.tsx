76c09d1e7bc6053f6f51e062b960576d10e9b9b9
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-05-17 15:22:43 +0200

    feat: add my career with a timeline

---
+++ src/components/career/career.tsx
@@ -0,0 +1,54 @@
+export default function Career(){
+    return (
+        <div className="min-h-[60vh] lg:min-h-[40vh] pt-10 flex flex-col items-center overflow-hidden">
+            <ul className="timeline timeline-vertical lg:timeline-horizontal w-full">
+                <li className="flex-1">
+                    <div className="timeline-end text-xl">2021 - 2024</div>
+                    <div className="timeline-middle">
+                        <div aria-label="info" className="status status-primary status-xl"></div>
+                    </div>
+                    <div className="timeline-start timeline-box text-center">
+                        <h1 className="text-xl font-poppins font-bold py-2">Baccalauréat Général</h1>
+                        <p className="text-base py-1">Lycée Léonard de Vinci - Soissons</p>
+                    </div>
+                    <hr className="bg-gray-600"/>
+                </li>
+                <li className="flex-1">
+                    <hr className="bg-gray-600"/>
+                    <div className="timeline-start text-xl">2024 - ...</div>
+                    <div className="timeline-middle">
+                        <div aria-label="info" className="status status-primary status-xl"></div>
+                    </div>
+                    <div className="timeline-end timeline-box text-center">
+                        <h1 className="text-xl font-poppins font-bold py-2">BUT Informatique</h1>
+                        <p className="text-base py-1">IUT RCC - Reims</p>
+                    </div>
+                    <hr className="bg-gray-600"/>
+                </li>
+                <li className="flex-1">
+                    <hr className="bg-gray-600"/>
+                    <div className="timeline-end text-xl">07/2025 - 08/2025</div>
+                    <div className="timeline-middle">
+                        <div aria-label="info" className="status status-primary status-xl"></div>
+                    </div>
+                    <div className="timeline-start timeline-box text-center">
+                        <h1 className="text-xl font-poppins font-bold py-2">Agent Pépiniériste</h1>
+                        <p className="text-base py-1">Dalival - Villers-Cotterêts</p>
+                    </div>
+                    <hr className="bg-gray-600"/>
+                </li>
+                <li className="flex-1">
+                    <hr className="bg-gray-600"/>
+                    <div className="timeline-start text-xl">04/2026 - 05/2026</div>
+                    <div className="timeline-middle">
+                        <div aria-label="info" className="status status-primary status-xl"></div>
+                    </div>
+                    <div className="timeline-end timeline-box text-center">
+                        <h1 className="text-xl font-poppins font-bold py-2">Stage Symfony</h1>
+                        <p className="text-base py-1">Olihost - Soissons</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
